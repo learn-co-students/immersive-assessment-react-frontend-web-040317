e7db27312f7c3daf2829e25ce643565b009ee40c
jonathanandrewsuk <jonathanandrewsuk@gmail.com>
2017-06-16 10:56:28 -0400

    format transaction amount

--- src/components/Transaction.js
+++ src/components/Transaction.js
@@ -1,16 +1,19 @@
 import React from 'react'
 
+
 const Transaction = (props) => {
 
-  var options = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric', hour: '2-digit', minute: "2-digit" };
-  let dateConvert = new Date(props.transaction.posted_at).toLocaleString('en-US', options) //Cleaned up the date/time
+  let options = { weekday: 'short', year: 'numeric', month: '2-digit', day: '2-digit', hour: '2-digit', minute: "2-digit" };
+  let dateConvert = new Date(props.transaction.posted_at).toLocaleString('en-US', options)
+  let cashConvert = props.transaction.amount.toLocaleString()
+    //Cleaned up the posted_at/amount, (all transactions happen at same time of day in API data)
 
   return (
     <tr>
       <td>{dateConvert}</td>
       <td>{props.transaction.description}</td>
       <td>{props.transaction.category}</td>
-      <td>{props.transaction.amount}</td>
+      <td>${cashConvert}</td>
     </tr>
   )
 }
