b679a3b232e9e4dbbade5afd1eacfcaa5df61241
github username <github email address>
2017-06-16 10:12:25 -0400

    full functionality working. Now to try to throttle search results

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,11 +1,15 @@
 import React from 'react'
 
-const Search = () => {
+const Search = (props) => {
+  function onChange(event){
+    props.handleChange(event)
+  }
   return (
     <div className="ui huge fluid icon input">
       <input
         type="text"
         placeholder={"Search your Recent Transactions"}
+        onChange={onChange}
       />
       <i className="circular search link icon"></i>
     </div>
--- src/components/Transaction.js
+++ src/components/Transaction.js
@@ -1,12 +1,27 @@
 import React from 'react'
 
-const Transaction = () => {
+const Transaction = (props) => {
+  if (props.searchTerm !== ''){
+    if (props.transaction.description.includes(props.searchTerm) ||
+      props.transaction.category.includes(props.searchTerm)) {
+        return (
+          <tr>
+            <td>{props.transaction.posted_at}</td>
+            <td>{props.transaction.description}</td>
+            <td>{props.transaction.category}</td>
+            <td>{props.transaction.amount}</td>
+          </tr>
+        )
+      } else {
+        return null
+      }
+  }
   return (
     <tr>
-      <td>{"...your code here"}</td>
-      <td>{"...your code here"}</td>
-      <td>{"...your code here"}</td>
-      <td>{"...your code here"}</td>
+      <td>{props.transaction.posted_at}</td>
+      <td>{props.transaction.description}</td>
+      <td>{props.transaction.category}</td>
+      <td>{props.transaction.amount}</td>
     </tr>
   )
 }
