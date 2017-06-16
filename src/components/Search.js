e69953109323c0f7aa5653b3d1e2f86dee23c7f3
iwpeifer <iwpeifer@gmail.com>
2017-06-16 10:52:37 -0400

    🍀

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,9 +1,10 @@
 import React from 'react'
 
-const Search = () => {
+const Search = (props) => {
   return (
     <div className="ui huge fluid icon input">
       <input
+        onChange={props.handleChange}
         type="text"
         placeholder={"Search your Recent Transactions"}
       />
