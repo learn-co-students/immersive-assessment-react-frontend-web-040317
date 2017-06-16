30b114ffe79b5b7a10ffac4c3c744a8c3103f76c
kylewstewart <kyle.w.stewart@mac.com>
2017-06-16 11:06:34 -0400

    no love for search

--- src/components/TransactionsList.js
+++ src/components/TransactionsList.js
@@ -1,38 +1,79 @@
-import React from 'react'
+import React, { Component } from 'react'
 
-const TransactionsList = () => {
+export default class TransactionsList extends Component {
 
-  return (
-    <table className="ui celled striped padded table">
-      <tbody>
-        <tr>
-          <th>
-            <h3 className="ui center aligned header">
-              Posted At
-            </h3>
-          </th>
-          <th>
-            <h3 className="ui center aligned header">
-              Description
-            </h3>
-          </th>
-          <th>
-            <h3 className="ui center aligned header">
-              Category
-            </h3>
-          </th>
-          <th>
-            <h3 className="ui center aligned header">
-              Amount
-            </h3>
-          </th>
-        </tr>
+  constructor(props){
+    super(props)
 
-        {"... your code here..."}
+    this.state = {
+      transactions: props.transactions
+    }
+  }
 
-      </tbody>
-    </table>
-  )
+  componentWillReceiveProps(nextProps) {
+    this.setState({transactions: nextProps.transactions})
+
+    // if (!nextProps.searchTerm) {
+    //   this.setState({transactions: nextProps.transactions})
+    // } else {
+    //   this.setState( prevState => {
+    //     {transactions: prevState.transactions.map(function(t){
+    //
+    //     }
+    //
+    //     )}
+    //   })
+    // }
+  }
+
+
+
+  render() {
+
+    return (
+      <table className="ui celled striped padded table">
+        <tbody>
+          <tr>
+            <th>
+              <h3 className="ui center aligned header">
+                Posted At
+              </h3>
+            </th>
+            <th>
+              <h3 className="ui center aligned header">
+                Description
+              </h3>
+            </th>
+            <th>
+              <h3 className="ui center aligned header">
+                Category
+              </h3>
+            </th>
+            <th>
+              <h3 className="ui center aligned header">
+                Amount
+              </h3>
+            </th>
+          </tr>
+          {this.state.transactions.map(transaction => {
+            return (
+              <tr> <td>
+                {transaction.posted_at}
+              </td>
+              <td>
+                {transaction.description}
+              </td>
+              <td>
+                {transaction.category}
+              </td>
+              <td>
+                {transaction.amount}
+              </td>
+            </tr>
+            )}
+          )}
+        </tbody>
+      </table>
+    )
+  }
 }
-
-export default TransactionsList
