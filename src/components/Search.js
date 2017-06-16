3361b15d2dec341c676fe40d622132cf7ad763e2
github username <github email address>
2017-06-16 10:22:48 -0400

    tried to use lodash for some fancy effects but no such luck

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,5 +1,13 @@
 import React from 'react'
 
+// const searchTerm=_.debounce((term) =>
+//   {this.searchTerm(term)}, 300);
+
+// tried to use lodash and debounce to throttle search results, but failed miserably.
+// really wanted to use throttled searches so the rendered results wouldn't have such
+// a jarring effect to the user. Oh well. tried my best.
+// Here's to hoping that I'll figure it out on my project!
+
 const Search = (props) => {
   function onChange(event){
     props.handleChange(event)
