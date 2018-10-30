81455511f4457c5d26c254007b55deb0c724da8a
Michael Cheng <cheng.c.mike@gmail.com>
2018-10-30 14:27:16 -0500

    commit

--- src/index.js
+++ src/index.js
@@ -1,10 +1,9 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './components/App';
-import './stylesheets/index.css';
-import 'semantic-ui-css/semantic.min.css'
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./components/App";
+import "./stylesheets/index.css";
+import "semantic-ui-css/semantic.min.css";
 
-ReactDOM.render(
-  <App />,
-  document.getElementById('root')
-);
+const blah = "blah";
+
+ReactDOM.render(<App />, document.getElementById("root"));
